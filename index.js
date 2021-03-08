8d77f7490f19c8334a69c8e1eedb787a3ef422a0
defor <deforworks@gmail.com>
2021-03-08 12:39:07 +0300

    Подправил структуру

--- index.js
+++ index.js
@@ -2,8 +2,9 @@ const express = require("express");
   
 const app = express();
 
-const PORT = process.env.PORT || 80
+const PORT = process.env.PORT || 3000
 
+app.use(express.static(__dirname + '/public'));
 app.get("/", function(request, response){
     response.sendFile(__dirname + "/index.html");
 });
