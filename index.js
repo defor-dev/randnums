f0a685e4e401ff2a0f5bca02097770bd16baf468
defor <deforworks@gmail.com>
2021-03-08 12:49:54 +0300

    Deploy version

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const app = express();
 
 const PORT = process.env.PORT || 80
 
-app.use(express.static(__dirname + '/public'));
 app.get("/", function(request, response){
     response.sendFile(__dirname + "/index.html");
 });
